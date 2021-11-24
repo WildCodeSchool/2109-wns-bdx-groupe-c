b22bc5e17f191fbeef6c6f48b7a37e61e0d0a683
Matthieu Dejean <matthieudejean2@gmail.com>
2021-11-24 16:58:57 +0100

    config

--- back/src/database-connection.ts
+++ back/src/database-connection.ts
@@ -1,11 +1,19 @@
-import {createConnection} from 'typeorm';
+import { createConnection } from 'typeorm'
 
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 const getDatabaseConnection = async () => {
-    await createConnection();
+  await createConnection({
+    type: 'mysql',
+    host: process.env.TYPEORM_HOST,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
+    port: parseInt(process.env.TYPEORM_PORT || '3306'),
+    ...(process.env.TYPEORM_SOCKETPATH && { socketPath: process.env.TYPEORM_SOCKETPATH }),
+  })
 }
 
-export default getDatabaseConnection;
+export default getDatabaseConnection
