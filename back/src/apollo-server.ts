66b57249a3950d7a370398554034d0868cf3cfdf
Alexis VIBERT <avibert@omnilog.fr>
2021-11-25 12:15:08 +0100

    add Task Entity with basic test

--- back/src/apollo-server.ts
+++ back/src/apollo-server.ts
@@ -1,5 +1,5 @@
-import { ApolloServer } from "apollo-server";
-import { buildSchema } from "type-graphql";
+import { ApolloServer } from 'apollo-server'
+import { buildSchema } from 'type-graphql'
 
 import ProjectResolver from './resolvers/ProjectResolver'
 import ProjectRoleResolver from './resolvers/ProjectRoleResolver'
@@ -8,17 +8,21 @@ import RoleResolver from './resolvers/RoleResolver'
 import CommentResolver from './resolvers/CommentResolver'
 import UserResolver from './resolvers/UserResolver'
 import LanguageResolver from './resolvers/LanguageResolver'
+import TaskResolver from './resolvers/TaskResolver'
 
 export default async () => {
-    const schema = await buildSchema({ resolvers: [
-        ProjectResolver,
-        ProjectRoleResolver,
-        StatusResolver,
-        RoleResolver,
-        CommentResolver,
-        UserResolver,
-        LanguageResolver
-    ] });
-    const server = new ApolloServer({ schema });
-    return server;
+  const schema = await buildSchema({
+    resolvers: [
+      ProjectResolver,
+      ProjectRoleResolver,
+      StatusResolver,
+      RoleResolver,
+      CommentResolver,
+      UserResolver,
+      LanguageResolver,
+      TaskResolver,
+    ],
+  })
+  const server = new ApolloServer({ schema })
+  return server
 }
