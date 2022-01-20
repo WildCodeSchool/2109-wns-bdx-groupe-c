ca18b1a430ad08d4dba4a507751351999b42411e
Alexis VIBERT <avibert@omnilog.fr>
2022-01-20 10:40:58 +0100

    [BACK] Update package.json in order to reload database with fixtures

---
+++ reload_fixtures.sh
@@ -0,0 +1,14 @@
+docker exec -it db_redlion mysql -u root -p -e "use redlion; SET FOREIGN_KEY_CHECKS=0;
+truncate table comment;
+truncate table language;
+truncate table project;
+truncate table project_language;
+truncate table project_role;
+truncate table role;
+truncate table status;
+truncate table task;
+truncate table user;
+truncate table user_project;
+SET FOREIGN_KEY_CHECKS=1;"
+echo "Database redlion cleaned"
+docker exec -it back_redlion npm run make:fixtures
